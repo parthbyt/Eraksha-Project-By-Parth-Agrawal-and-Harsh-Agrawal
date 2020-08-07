234bb5610623c087ec3539c9517fd8695b4401a2
Parth Agrawal <parthagrawal.byt.2007@gmail.com>
2020-08-07 16:55:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import requests
 
 eel.init('src')
 
-api_file = open("src\\news\\api.txt", "r")
-api = api_file.read()
-url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=" + api + "&pageSize=10"
-news_data = requests.get(url).json()
-
-eel.parseContent(news_data)
+try:
+    api_file = open("api.txt", "r")
+    api = api_file.read()
+    url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=" + api + "&pageSize=10"
+    news_data = requests.get(url).json()
+    eel.parseContent(news_data)
+except:
+    print("Newsapi not found. News will not be shown")
 
 eel.start('index.html', port=5010, size=(2500, 2500))
